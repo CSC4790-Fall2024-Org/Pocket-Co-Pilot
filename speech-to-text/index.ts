02693f04022819b100446af36b5341d2e6c11eaf
jackmcintyre333 <131042229+jackmcintyre333@users.noreply.github.com>
2024-09-29 21:10:34 -0400

    continue working on tutorial speech to text

--- speech-to-text/index.ts
+++ speech-to-text/index.ts
@@ -1,2 +1,21 @@
 import express from "express";
+import { speechToText } from "./functions/speechToText";
+import "dotenv/config";
+const port = process.env.PORT || 4000;
+const app = express();
+app.use(
+    express.json({
+    limit: "50mb",
+    })
+);
+app.post("/speech-to-text", (req: Request, res: Response) => {
+    speechToText(req, res);
+});
 
+app.get("/,", (req, res) => {
+    res.send("App is running")
+});
+
+app.listen(port, () => {
+    console.log(`Server is runnig on port ${port}`);
+});
