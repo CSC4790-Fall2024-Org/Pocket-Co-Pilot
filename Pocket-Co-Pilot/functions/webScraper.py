8a8b4e3b273f28bfa47ffa2467121f920ccd0d1d
Tdailey990 <tdailey@villanova.edu>
2024-10-25 12:36:09 -0400

    Outline webScraper

--- Pocket-Co-Pilot/functions/webScraper.py
+++ Pocket-Co-Pilot/functions/webScraper.py
@@ -1,14 +1,18 @@
-# Web Scraper format
-# Original: 3 AUG 2020 - Bell Labs, Murray Hill
-# Current: 14 FEB 2022 - Villanova University
-# Version: 1.0
-
 #Imports
 import requests
 from bs4 import BeautifulSoup
 
+#Takes in keyword and location strings
+
 #Get IP and URL
-ip = str(input('Enter the IP Adresses:\n'))
+# https://www.airnav.com/airport/kukt
+
+#Get Specific Query and Airport
+airport = "KUKT" 
+query = "runway length"
+
+#Build URL
+ip = str(input('www.airnav.com/airport/' + airport + '\n'))
 URL = 'https://' + ip + '/'
 print ('Parsing URL ' + URL + '\n')
 
@@ -16,8 +20,22 @@ print ('Parsing URL ' + URL + '\n')
 page = requests.get(URL)
 soup = BeautifulSoup(page.content, 'html.parser')
 
+#Get Informtion From Location
+match query:
+    case "runway length":
+        print("runway length")
+    case "elevation":
+        print("elevation")
+    case "weather":
+        print("weather")
+    case _:
+        print("Not Valid")
+
 #Find Header and Advanced Settings
 main_header = soup.find('div', class_='k-header-maintitle-div')
 adv_settings = soup.find(id='advancedSettingsDiv')
 
-#
+#Process Response
+
+#Returns string
+#return str(parsed_response)
