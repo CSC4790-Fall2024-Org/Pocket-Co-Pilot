0b5559dbed9f27aedfd3375439ca27d8bcf0e6ab
Tdailey990 <tdailey@villanova.edu>
2024-10-28 14:05:20 -0400

    Update webScraper

--- Pocket-Co-Pilot/functions/webScraper.py
+++ Pocket-Co-Pilot/functions/webScraper.py
@@ -6,13 +6,14 @@ from bs4 import BeautifulSoup
 
 #Get IP and URL
 # https://www.airnav.com/airport/kukt
+# https://acukwik.com/Airport-Info/KUKT 
 
 #Get Specific Query and Airport
 airport = "KUKT" 
 query = "runway length"
 
 #Build URL
-ip = str(input('www.airnav.com/airport/' + airport + '\n'))
+ip = str('acukwik.com/Airport-Info/' + airport)
 URL = 'https://' + ip + '/'
 print ('Parsing URL ' + URL + '\n')
 
@@ -20,14 +21,35 @@ print ('Parsing URL ' + URL + '\n')
 page = requests.get(URL)
 soup = BeautifulSoup(page.content, 'html.parser')
 
+returned_str = ""
+print(soup)
+
+#Find Header and Advanced Settings
+main_header = soup.find('div', class_='airportInfo')
+adv_settings = soup.find(id='advancedSettingsDiv')
+
+print(main_header)
+
 #Get Informtion From Location
 match query:
-    case "runway length":
-        print("runway length")
     case "elevation":
         print("elevation")
+
+    case "runway length":
+        print("runway length")
+
     case "weather":
         print("weather")
+
+    case "CTAF/UNICOM":
+        print("CTAF/UNICOM")
+
+    case "AWOS":
+        print("AWOS")
+
+    case "pattern":
+        print("traffic pattern")
+
     case _:
         print("Not Valid")
 
