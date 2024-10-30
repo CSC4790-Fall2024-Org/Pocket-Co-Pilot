2346025bfc5650c0530d62bc8ed6602f688cf078
Tdailey990 <tdailey@villanova.edu>
2024-10-30 13:52:11 -0400

    Update WebScraper

--- Pocket-Co-Pilot/functions/webScraper.py
+++ Pocket-Co-Pilot/functions/webScraper.py
@@ -10,52 +10,62 @@ from bs4 import BeautifulSoup
 
 #Get Specific Query and Airport
 airport = "KUKT" 
-query = "runway length"
+query = "weather"
 
 #Build URL
-ip = str('acukwik.com/Airport-Info/' + airport)
-URL = 'https://' + ip + '/'
-print ('Parsing URL ' + URL + '\n')
+if (query == 'weather'):
+    ip = str('acukwik.com/Weather/' + airport)
+    URL = 'https://' + ip + '/'
+
+else:
+    ip = str('acukwik.com/Airport-Info/' + airport)
+    URL = 'https://' + ip + '/'
+    print ('Parsing URL ' + URL + '\n')
 
 #Parse URL
 page = requests.get(URL)
 soup = BeautifulSoup(page.content, 'html.parser')
 
-returned_str = ""
-print(soup)
-
-#Find Header and Advanced Settings
-main_header = soup.find('div', class_='airportInfo')
-adv_settings = soup.find(id='advancedSettingsDiv')
-
-print(main_header)
 
 #Get Informtion From Location
 match query:
     case "elevation":
-        print("elevation")
+        #Get Elevation
+        elevation_label = soup.find('div', class_='clearboth p3xp bold', string=lambda text: "Elevation" in text.strip())
+        elevation_value = elevation_label.find_next_sibling('div', class_='clearboth p3px').text.strip()
+        print('The elevation of ' + airport + ' is: ' + elevation_value + ' feet')
 
     case "runway length":
-        print("runway length")
+        #Get Runway Length
+        runway_label = soup.find('div', class_='clearboth p3xp bold', string=lambda text: "Longest Primary Runway" in text.strip())
+        runway_value = runway_label.find_next_sibling('div', class_='clearboth p3px').text.strip()
+        print('The runway length of ' + airport + ' is: ' + runway_value)
 
     case "weather":
-        print("weather")
+        #Get Weather
+        weather_label = soup.find('div', class_='w10p fl bold', string=lambda text: "METAR" in text.strip())
+        weather_value = weather_label.find_next_sibling('div', class_='w90p fl').text.strip()
+        print(weather_value)
 
     case "CTAF/UNICOM":
-        print("CTAF/UNICOM")
+        #Parse URL
+        page = requests.get(URL)
+        soup = BeautifulSoup(page.content, 'html.parser')
+        
+        #Get CTAF/UNICOM
+        UNICOM_label = soup.find('div', class_='fl w35p bold', string=lambda text: "Frequency" in text.strip())
+        UNICOM_value = UNICOM_label.find_next_sibling('div', class_='fl w65p').text.strip()
+        print('The CTAF/UNICOM of ' + airport + ' is: ' + UNICOM_value)
 
     case "AWOS":
         print("AWOS")
-
-    case "pattern":
-        print("traffic pattern")
 
     case _:
         print("Not Valid")
 
 #Find Header and Advanced Settings
-main_header = soup.find('div', class_='k-header-maintitle-div')
-adv_settings = soup.find(id='advancedSettingsDiv')
+# main_header = soup.find('div', class_='clearfix TopAirportInfo')
+# adv_settings = soup.find(id='advancedSettingsDiv')
 
 #Process Response
 
